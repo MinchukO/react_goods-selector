f4bb20dec5a5e5b82bbe2ac02c0a7edf626bb0c2
Minchuk Olga <olga@MacBook-Pro-Olga.local>
2021-05-25 12:12:31 +0300

    Updates

---
+++ precache-manifest.cbd94f960f8640ba5dbdebf31904e9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6dad15de2d2d90675b2cdd5ad275dc0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cfac6ce0952e7b88392d",
+    "url": "./static/css/main.d7c26701.chunk.css"
+  },
+  {
+    "revision": "c07500df0ccde94f4968",
+    "url": "./static/js/2.f3cc3ca8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f3cc3ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfac6ce0952e7b88392d",
+    "url": "./static/js/main.80d547e3.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
